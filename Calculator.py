a8854a2ac95e15a38031b4ef61602c9c6b2658cf
Callan <cjansen4@asu.edu>
2019-02-28 17:49:32 -0700

    Added basic number operations. Takes input from user and prints the result of the operation.

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,15 @@
-print("Test github commit")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation: +, -, *, /")
+
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2)
+elif operation == "/":
+    print(num1 / num2)
+else:
+    print("Invalid input")
